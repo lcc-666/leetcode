39ff59369b9579135d31da2b170d6b38459c85db
2502247952@qq.com <2502247952@qq.com>
2022-03-03 21:35:51 +0800

    合并两个有序链表

---
+++ two_week/Merge Two Sorted Lists.py
@@ -0,0 +1,36 @@
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+
+示例 2：
+输入：l1 = [], l2 = []
+输出：[]
+
+示例 3：
+输入：l1 = [], l2 = [0]
+输出：[0]
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self):
+        # 无法模拟链表，只记录算法
+        # if list1 is None:
+        #     return list2
+        # elif list2 is None:
+        #     return list1
+        # elif list1.val < list2.val:
+        #     list1.next = self.mergeTwoLists(list1.next, list2)
+        #     return list1
+        # else:
+        #     list2.next = self.mergeTwoLists(list1, list2.next)
+        #     return list2
+        pass
