9b3d10ad3a1793884b859c80cad5739dca5aeebf
2502247952@qq.com <2502247952@qq.com>
2022-03-11 23:43:02 +0800

    找到字符串中所有字母异位词

---
+++ three_week/Find All Anagrams in a String.py
@@ -0,0 +1,56 @@
+"""
+给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+
+异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+
+示例 1=:
+输入: s = "cbaebabacd", p = "abc"
+输出: [0,6]
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+
+示例 2:
+输入: s = "abab", p = "ab"
+输出: [0,1,2]
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.s = "cbaebabacd"
+        self.p = "abc"
+
+    def findAnagrams(self):
+        s = self.s
+        p = self.p
+        # 算法部分
+        s_len = len(s)
+        p_len = len(p)
+        if s_len < p_len:
+            return []
+        ans = []
+        s_count = [0] * 26
+        p_count = [0] * 26
+        for i in range(p_len):
+            s_count[ord(s[i]) - 97] += 1
+            p_count[ord(p[i]) - 97] += 1
+        if s_count == p_count:
+            ans.append(0)
+
+        for i in range(s_len-p_len):
+            s_count[ord(s[i]) - 97] -= 1
+            s_count[ord(s[i + p_len]) - 97] += 1
+            if s_count == p_count:
+                ans.append(i + 1)
+
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.findAnagrams()
