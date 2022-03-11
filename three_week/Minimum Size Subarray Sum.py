e43fd2c3084c081a318985b19da01b1b2d16486b
2502247952@qq.com <2502247952@qq.com>
2022-03-12 00:28:18 +0800

    长度最小的子数组

---
+++ three_week/Minimum Size Subarray Sum.py
@@ -0,0 +1,44 @@
+"""
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+示例 1：
+
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+
+示例 2：
+输入：target = 4, nums = [1,4,4]
+输出：1
+
+示例 3：
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.target = 7
+        self.nums = [2, 3, 1, 2, 4, 3]
+
+    def minSubArrayLen(self):
+        target = self.target
+        nums = self.nums
+        # 算法逻辑
+        if not nums:
+            return 0
+        n = len(nums)
+        ans = n + 1
+        start, end = 0, 0
+        total = 0
+        while end < n:
+            total += nums[end]
+            while total >= target:
+                ans = min(ans, end - start + 1)
+                total -= nums[start]
+                start += 1
+            end += 1
+        return 0 if ans == n + 1 else ans
