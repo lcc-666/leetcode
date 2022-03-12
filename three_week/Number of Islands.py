669e904961c6cf2faaf580db2db53f5d16c4853c
2502247952@qq.com <2502247952@qq.com>
2022-03-12 22:13:11 +0800

    岛屿数量

---
+++ three_week/Number of Islands.py
@@ -0,0 +1,65 @@
+"""
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+
+示例 1：
+
+输入：grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+输出：1
+示例 2：
+
+输入：grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+输出：3
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.grid = [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]
+        ]
+
+    def dfs(self, grid, r, c):
+        grid[r][c] = 0
+        nr = len(grid)
+        nc = len(grid[0])
+        for x, y in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+            if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
+                self.dfs(grid, x, y)
+
+    def numIslands(self):
+        grid = self.grid
+        # 算法部分
+        nr = len(grid)
+        nc = len(grid[0])
+        num_islands = 0
+        if nr == 0:
+            return 0
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    num_islands += 1
+                    self.dfs(grid, r, c)
+
+        return num_islands
+
+if __name__ == '__main__':
+    s=Solution()
+    p=s.numIslands()
+    print(p)
