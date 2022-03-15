907384010b807c9b89eedd5e28e70b00d3381bbe
2502247952@qq.com <2502247952@qq.com>
2022-03-15 23:01:31 +0800

    所有可能的路径

---
+++ four_week/All Paths From Source to Target.py
@@ -0,0 +1,39 @@
+"""
+给你一个有 n 个节点的 有向无环图（DAG），请你找出所有从节点 0 到节点 n-1 的路径并输出（不要求按特定顺序）
+
+graph[i] 是一个从节点 i 可以访问的所有节点的列表（即从节点 i 到节点 graph[i][j]存在一条有向边）。
+
+示例 1：
+
+输入：graph = [[1,2],[3],[3],[]]
+输出：[[0,1,3],[0,2,3]]
+解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+示例 2：
+
+输入：graph = [[4,3,1],[3,2,4],[3],[4],[]]
+输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.graph = [[1, 2], [3], [3], []]
+
+    def allPathsSourceTarget(self):
+        graph = self.graph
+        # 算法目录
+        ans = []
+        stk = []
+
+        def dfs(x):
+            if x == len(graph) - 1:
+                ans.append(stk[:])
+                return
+            for y in graph[x]:
+                stk.append(y)
+                dfs(y)
+                stk.pop()
+
+        stk.append(0)
+        dfs(0)
+        return ans
