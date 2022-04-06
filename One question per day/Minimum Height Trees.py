82774bbf83a9fdde0513970717fe4d856bc4acc2
lcc-666 <2502247952@qq.com>
2022-04-06 19:13:09 +0800

    最小高度树

---
+++ One question per day/Minimum Height Trees.py
@@ -0,0 +1,69 @@
+"""
+树是一个无向图，其中任何两个顶点只通过一条路径连接。 换句话说，一个任何没有简单环路的连通图都是一棵树。
+
+给你一棵包含n个节点的树，标记为0到n - 1 。给定数字n和一个有 n - 1 条无向边的 edges列表（每一个边都是一对标签），其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条无向边。
+
+可选择树中任何一个节点作为根。当选择节点 x 作为根节点时，设结果树的高度为 h 。在所有可能的树中，具有最小高度的树（即，min(h)）被称为 最小高度树 。
+
+请你找到所有的 最小高度树 并按 任意顺序 返回它们的根节点标签列表。
+
+树的 高度 是指根节点和叶子节点之间最长向下路径上边的数量。
+
+示例 1：
+
+
+输入：n = 4, edges = [[1,0],[1,2],[1,3]]
+输出：[1]
+解释：如图所示，当根是标签为 1 的节点时，树的高度是 1 ，这是唯一的最小高度树。
+示例 2：
+
+
+输入：n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+输出：[3,4]
+"""
+from collections import deque
+
+
+class Solution:
+    def __init__(self):
+        self.n = 6
+        self.edges = [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]
+
+    def findMinHeightTrees(self):
+        n = self.n
+        edges = self.edges
+        #算法部分
+        if n == 1:
+            return [0]
+
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+        parents = [0] * n
+
+        def bfs(start: int):
+            vis = [False] * n
+            vis[start] = True
+            q = deque([start])
+            while q:
+                x = q.popleft()
+                for y in g[x]:
+                    if not vis[y]:
+                        vis[y] = True
+                        parents[y] = x
+                        q.append(y)
+            return x
+
+        x = bfs(0)  # 找到与节点 0 最远的节点 x
+        y = bfs(x)  # 找到与节点 x 最远的节点 y
+
+        path = []
+        parents[x] = -1
+        while y != -1:
+            path.append(y)
+            y = parents[y]
+        m = len(path)
+        return [path[m // 2]] if m % 2 else [path[m // 2 - 1], path[m // 2]]
+
+
