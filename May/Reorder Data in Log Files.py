f82d3e861f2f670168a0814285016a8f1dec8147
lcc-666 <2502247952@qq.com>
2022-05-03 19:54:59 +0800

    重新排列日志文件

---
+++ May/Reorder Data in Log Files.py
@@ -0,0 +1,25 @@
+"""
+给你一个日志数组 logs。每条日志都是以空格分隔的字串，其第一个字为字母与数字混合的 标识符 。
+
+有两种不同类型的日志：
+
+字母日志：除标识符之外，所有字均由小写字母组成
+数字日志：除标识符之外，所有字均由数字组成
+请按下述规则将日志重新排序：
+
+所有 字母日志 都排在 数字日志 之前。
+字母日志 在内容不同时，忽略标识符后，按内容字母顺序排序；在内容相同时，按标识符排序。
+数字日志 应该保留原来的相对顺序。
+返回日志的最终顺序。
+"""
+from typing import List
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def trans(log: str) -> tuple:
+            a, b = log.split(' ', 1)
+            return (0, b, a) if b[0].isalpha() else (1,)
+
+        logs.sort(key=trans)  # sort 是稳定排序
+        return logs
